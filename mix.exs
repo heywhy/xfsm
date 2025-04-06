5c56cccd8c407e6d9b122c31beff30fab016c4f2
atanda rasheed <atandarash@gmail.com>
2025-04-06 13:34:41 +0100

    ci: setup gh workflows

--- mix.exs
+++ mix.exs
@@ -43,9 +43,11 @@ defmodule XFsm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:castore, "~> 1.0", only: [:test]},
       {:credo, "~> 1.7", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18", only: [:test]},
       {:git_hooks, "~> 0.8", only: :dev, runtime: false},
       {:git_ops, "~> 2.6", only: :dev, runtime: false}
     ]
